68f11bc7666f7fed1e3bdcdc27027a2049604adb
Lazy Cat <mylazysundaycat@gmail.com>
2024-02-01 11:17:38 +0900

    섹션1 타임리프 HTTP 엔티티, 이스케이프, 변수 규칙

--- src/main/java/hello/thymeleaf/ThymeleafBasicApplication.java
+++ src/main/java/hello/thymeleaf/ThymeleafBasicApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ThymeleafBasicApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ThymeleafBasicApplication.class, args);
 	}
